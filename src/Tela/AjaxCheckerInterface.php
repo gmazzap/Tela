5a0e4618b380bde60f3127f9db43f32a75e8d87a
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-09-06 16:36:10 +0200

    Docbloc for AjaxCheckerInterface

--- src/Tela/AjaxCheckerInterface.php
+++ src/Tela/AjaxCheckerInterface.php
@@ -2,15 +2,46 @@
 
 interface AjaxCheckerInterface {
 
+    /**
+     * Getter for the request array.
+     *
+     * @return array
+     */
     public function getRequest();
 
+    /**
+     * Getter for the action object.
+     *
+     * @return \GM\Tela\ActionInterface
+     */
     public function getAction();
 
+    /**
+     * Setter for the request array.
+     *
+     * @param array $request
+     */
     public function setRequest( Array $request );
 
+    /**
+     * Setter for the action object.
+     *
+     * @param \GM\Tela\ActionInterface $action
+     */
     public function setAction( ActionInterface $action );
 
+    /**
+     * Check sanity of current request and action
+     *
+     * @param array $request
+     */
     public function checkRequest();
 
+    /**
+     * Check action nonce against a salt,
+     *
+     * @param string $salt
+     * @return boolean
+     */
     public function checkNonce( $nonce, $salt );
 }
