de662b6ecca43dbd9ed439d3328f89e17261fd7c
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-09-06 16:36:54 +0200

    Minor changes in AjaxChecker class

--- src/Tela/AjaxChecker.php
+++ src/Tela/AjaxChecker.php
@@ -23,45 +23,39 @@ class AjaxChecker implements AjaxCheckerInterface {
     }
 
     public function setRequest( Array $request ) {
-        $this->request = $request;
+        $keys = [ 'is_tela', 'action', 'nonce', 'blogid', 'from_admin' ];
+        $this->request = wp_parse_args( $request, array_fill_keys( $keys, NULL ) );
     }
 
     public function checkRequest() {
         return $this->checkUrlVars() && $this->checkAction() && $this->checkSide();
     }
 
-    public function checkNonce( $nonce, $salt ) {
-        $check = preg_replace( "#^{$salt}#", '', base64_decode( $nonce ), 1 );
+    public function checkNonce( $salt ) {
+        $check = preg_replace( "#^{$salt}#", '', base64_decode( $this->getAction()->getNonce() ), 1 );
         return (bool) wp_verify_nonce( $check, $this->request[ 'action' ] );
     }
 
-    /**
-     * Check if http request ($_GET and $_POST) contains valid Tela-related variables.
-     *
-     * @param array $vars Var array to check, if not given is retrieved from superglobals
-     * @return boolean
-     * @access private
-     */
     private function checkUrlVars() {
         $request = $this->getRequest();
-        return $request[ 'is_tela' ]
-            && ! empty( $request[ 'action' ] )
-            && ! empty( $request[ 'nonce' ] )
+        return $request [ 'is_tela' ]
+            && ! empty( $request [ 'action' ] )
+            && ! empty( $request [ 'nonce' ] )
             && ! empty( $request[ 'blogid' ] );
     }
 
     private function checkAction() {
         $action = $this->getAction();
-		$request = $this->getRequest();     
-        return ( $action->getVar( 'public' ) || is_user_logged_in() )
-			&& $action->getBlogId() === $request[ 'blogid' ];
+        $request = $this->getRequest();
+        return ( $action->isPublic() || is_user_logged_in() )
+            && $action->getBlogId() === $request[ 'blogid' ];
     }
 
     private function checkSide() {
-        $action = $this->getAction();
-		$request = $this->getRequest();
-		$bad_side = $request['from_admin'] ? \GM\Tela::FRONTEND : \GM\Tela::BACKEND;
-        return $this->action->getVar( 'side' ) !== $bad_side;
+        $request = $this->getRequest();
+        $bad_side = $request[ 'from_admin' ] ? \GM\Tela::FRONTEND : \GM\Tela::BACKEND;
+        $action_side = $this->getAction()->getVar( 'side' );
+        return (int) $action_side > 0 && $action_side !== $bad_side;
     }
 
 }
