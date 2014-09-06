5a99aa7f9d2121b81eb8d11c5210f2d7d2a7b176
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-09-06 20:22:32 +0200

    Minor change on ActionArgsValidator

--- src/Tela/ActionArgsValidator.php
+++ src/Tela/ActionArgsValidator.php
@@ -3,38 +3,42 @@
 class ActionArgsValidator implements ActionArgsValidatorInterface {
 
     public function validate( Array $args ) {
-		$set = FALSE;
-		if ( ! isset( $args[ 'side' ] ) ) {
+        $valid_sides = [ \GM\Tela::BACKEND, \GM\Tela::FRONTEND, \GM\Tela::BOTHSIDES ];
+        $all_sides = $this->getSides();
+        $set = FALSE;
+        if ( ! isset( $args[ 'side' ] ) ) {
             $args[ 'side' ] = \GM\Tela::BACKEND;
-			$set = TRUE;
+            $set = TRUE;
         }
-        $sides = [
-            'backend'   =>  \GM\Tela::BACKEND,
-            'back'      =>  \GM\Tela::BACKEND,
-            'admin'     =>  \GM\Tela::BACKEND,
-            'frontend'  =>  \GM\Tela::FRONTEND,
-            'front'     =>  \GM\Tela::FRONTEND,
-            'public'    =>  \GM\Tela::FRONTEND,
-            'both'      =>  \GM\Tela::BOTHSIDES,
-            'bothsides' =>  \GM\Tela::BOTHSIDES,
-            'all'       =>  \GM\Tela::BOTHSIDES,
-            'any'       =>  \GM\Tela::BOTHSIDES,
-            '*'         =>  \GM\Tela::BOTHSIDES
-        ];
-		$valid = [ \GM\Tela::BACKEND, \GM\Tela::FRONTEND, \GM\Tela::BOTHSIDES ];
-        if ( ! $set && array_key_exists( strtolower( $args[ 'side' ] ), $sides ) ) {
-            $args[ 'side' ] = $sides[ strtolower( $args[ 'side' ] ) ];
-			$set = TRUE;
+        if ( ! $set && array_key_exists( strtolower( $args[ 'side' ] ), $all_sides ) ) {
+            $args[ 'side' ] = $all_sides[ strtolower( $args[ 'side' ] ) ];
+            $set = TRUE;
         }
-		if ( ! $set && ! in_array( $args[ 'side' ], $valid, TRUE ) ) {
-			$args[ 'side' ] = \GM\Tela::BACKEND;
-		}
+        if ( ! $set && ! in_array( $args[ 'side' ], $valid_sides, TRUE ) ) {
+            $args[ 'side' ] = \GM\Tela::BACKEND;
+        }
         if ( ! isset( $args[ 'public' ] ) ) {
             $args[ 'public' ] = FALSE;
         } elseif ( (bool) $args[ 'public' ] === TRUE ) {
-            $args[ 'side' ] =  \GM\Tela::FRONTEND;
+            $args[ 'side' ] = \GM\Tela::FRONTEND;
         }
         return $args;
+    }
+
+    public function getSides() {
+        return [
+            'backend'   => \GM\Tela::BACKEND,
+            'back'      => \GM\Tela::BACKEND,
+            'admin'     => \GM\Tela::BACKEND,
+            'frontend'  => \GM\Tela::FRONTEND,
+            'front'     => \GM\Tela::FRONTEND,
+            'public'    => \GM\Tela::FRONTEND,
+            'both'      => \GM\Tela::BOTHSIDES,
+            'bothsides' => \GM\Tela::BOTHSIDES,
+            'all'       => \GM\Tela::BOTHSIDES,
+            'any'       => \GM\Tela::BOTHSIDES,
+            '*'         => \GM\Tela::BOTHSIDES
+        ];
     }
 
 }
